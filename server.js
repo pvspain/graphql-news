6b138787c00ecf679442cb1fceb5245fdceefef8
Paul Spain <pvspain@gmail.com>
2018-08-25 23:20:44 +1000

    formatting. nfc

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
-import {makeExecutableSchema } from 'graphql-tools';
+import { makeExecutableSchema } from 'graphql-tools';
 
 const app = express();
 
