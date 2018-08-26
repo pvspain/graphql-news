7e43d6fd0e9333fa58006e3bb79cff6e64d19ce2
Paul Spain <pvspain@gmail.com>
2018-08-26 18:13:33 +1000

    end of recursive relationships. working

--- server.js
+++ server.js
@@ -10,33 +10,60 @@ const typeDefs = `
     type Link {
         id: Int! @unique
         url: String!
-        description: String!
+        description: String
         author: User!
+        comments: [Comment]
     }
 
     type User {
         id: Int! @unique
         username: String!
-        about: String
+        about: String!
+    }
+
+    type Comment {
+        id: Int! @unique
+        parent: Comment
+        comments: [Comment]
+        author: User!
+        content: String!   
     }
 
     type Query {
         allLinks: [Link]
         link(id: Int!): Link
         allUsers: [User]
-        user(id: Int!): User 
+        user(id: Int!): User
+        allComments: [Comment] 
     }
 `;
 
 const links = [
-    { id: 0, author: 0, url: 'https://google.com', description: 'Google' },
-    { id: 1, author: 1, url: 'https://github.com', description: 'GitHub' }
+    { id: 0, author: 0, url: 'https://google.com', description: 'Google', comments: [0, 4] },
+    { id: 1, author: 1, url: 'https://github.com', description: 'GitHub' },
 ];
 
 const users = [
     { id: 0, username: 'user1', about: 'The first user'},
-    { id: 1, username: 'user2', about: 'The second user'}
+    { id: 1, username: 'user2', about: 'The second user'},
+    { id: 2, username: 'user3', about: 'The third user'},
 ];
+
+const commentsList = [
+    { id: 0, parent: null, author: 0, content: 'Comment 1' },
+    { id: 1, parent: 0, author: 1, content: 'Comment 2' },
+    { id: 2, parent: 1, author: 0, content: 'Comment 3' },
+    { id: 3, parent: 0, author: 2, content: 'Comment 4' },
+    { id: 4, parent: null, author: 2, content: 'Comment 5' },
+];
+
+function getComments(commentID) {
+    const comments = commentsList.filter(comment => comment.parent === commentID);
+    if (comments.length > 0) {
+        return comments;
+    }
+    return null;
+}
 
 const resolvers = {
     Query: {
@@ -44,10 +71,16 @@ const resolvers = {
         link: (_, { id }) => find(links, { id }),
         allUsers: () => users,
         user: (_, { id }) => find(users, { id }),
+        allComments: () => commentsList,
     },
     Link: {
-        author: ({ author }) => find(users, { id: author })
-    }
+        author: ({ author }) => find(users, { id: author }),
+        comments: ({ comments }) => comments.map(i => find(commentsList, { id: i })),
+    },
+    Comment: {
+        author: ({ author }) => find(users, { id: author }), 
+        comments: ({ id }) => getComments(id),
+    },
 };
 
 const schema = makeExecutableSchema({
